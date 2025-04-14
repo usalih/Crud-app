915d3b3183ff08e4de42b33aadb155bd7aa1c45a
PAPPY <ussalih@yahoo.com>
2025-04-14 15:01:17 +0100

    DBconnect
    connecting database

--- server.js
+++ server.js
@@ -4,32 +4,32 @@ const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 
-const connectDB = require('./server/database/connection');
 
-const app = express();
+const routes  = require('./server/routers/router');
 
 // configure environment
 dotenv.config({path: 'config.env'})
-const PORT = process.env.PORT || 5050
 
 // log requests
 app.use(morgan('tiny'));
 
 // mongodb Connection
-connectDB();
+//connectDB();
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
-app.set("view engine", "ejs")
-//app.set("views", path.resolve(__dirname, "views/ejs"))
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
 
 //load assets
 app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
 
 //load routers
-app.use('/', require('.server/routers/router'))
+app.use('/', routes);
 
+//Start the server
+const PORT = process.env.PORT || 5050
 app.listen(PORT, ()=> {console.log('server in running on port: ', {PORT})});
