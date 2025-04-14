d4821d3b14f72ba623800194d425fdd89d2e1474
PAPPY <ussalih@yahoo.com>
2025-04-14 15:03:10 +0100

    DBconnect to server
    database connections

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
+const app = express();
 
+//const connectDB = require('./server/database/connection');
 
 const routes  = require('./server/routers/router');
 
