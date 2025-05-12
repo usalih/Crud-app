a06cddc5d33f6ad5e6dca905b343037691ce4c60
PAPPY <ussalih@yahoo.com>
2025-05-12 17:47:39 +0100

    Encoding sorted

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+// const express = require('express');
 const express = require('express');
+const PORT = process.env.PORT || 5050;
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const bodyparser = require("body-parser");
 const path = require('path');
 const app = express();
 
-//const connectDB = require('./server/database/connection');
+const connectDB = require('./server/database/connection');
 
-const routes  = require('./server/routers/router');
+const route  = require('./server/routers/router');
 
 // configure environment
 dotenv.config({path: 'config.env'})
@@ -16,13 +20,13 @@ dotenv.config({path: 'config.env'})
 app.use(morgan('tiny'));
 
 // mongodb Connection
-//connectDB();
+connectDB();
 
 // parse request to body-parser
 app.use(bodyparser.urlencoded({ extended : true}))
 
 // set view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 //load assets
@@ -30,8 +34,7 @@ app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
 app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
 
 //load routers
-app.use('/', routes);
+app.use('/', route);
 
 //Start the server
-const PORT = process.env.PORT || 5050
 app.listen(PORT, ()=> {console.log('server in running on port: ', {PORT})});
